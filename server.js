09513d794e6d1224f23dd0f1e92cf636ef3c7ef2
Murat Ayfer <murat@ayfer.net>
2014-10-15 23:01:09 -0700

    node connects

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
-connect().use(serveStatic(__dirname)).listen(8080);
+var path = require('path');
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+app.use(express.static(__dirname));
+
